7f57c1ca4f190b9b637923cd583b79f94c2ffc6d
Maxim <xxmaxdrivezz@gmail.com>
2018-04-04 01:19:34 +0300

    sources

---
+++ Assets/AR/Scripts/ARController.cs
@@ -0,0 +1,416 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ARController.cs" company="Google">
+//
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using InstantPreviewInput = GoogleARCore.InstantPreviewInput;
+
+namespace AR
+{
+    using System.Collections.Generic;
+    using GoogleARCore;
+    using UnityEngine;
+    using UnityEngine.Rendering;
+
+#if UNITY_EDITOR
+    using Input = InstantPreviewInput;
+#endif
+
+    public class ARController : MonoBehaviour
+    {
+        #region Constants
+
+        private float hitLength = 1000f;
+        #endregion
+
+        #region Variables
+        /// <summary>
+        /// The first-person camera being used to render the passthrough camera image (i.e. AR background).
+        /// </summary>
+        public Camera FirstPersonCamera;
+
+        /// <summary>
+        /// A prefab for tracking and visualizing detected planes.
+        /// </summary>
+        public GameObject TrackedPlanePrefab;
+
+        /// <summary>
+        /// A model to place when a raycast from a user touch hits a plane.
+        /// </summary>
+        public GameObject AndyAndroidPrefab;
+
+        /// <summary>
+        /// A gameobject parenting UI for displaying the "searching for planes" snackbar.
+        /// </summary>
+        public GameObject SearchingForPlaneUI;
+
+        /// <summary>
+        /// A list to hold new planes ARCore began tracking in the current frame. This object is used across
+        /// the application to avoid per-frame allocations.
+        /// </summary>
+        private List<TrackedPlane> m_NewPlanes = new List<TrackedPlane>();
+
+        /// <summary>
+        /// A list to hold all planes ARCore is tracking in the current frame. This object is used across
+        /// the application to avoid per-frame allocations.
+        /// </summary>
+        private List<TrackedPlane> m_AllPlanes = new List<TrackedPlane>();
+
+        /// <summary>
+        /// True if the app is in the process of quitting due to an ARCore connection error, otherwise false.
+        /// </summary>
+        private bool m_IsQuitting = false;
+
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+                                          TrackableHitFlags.FeaturePointWithSurfaceNormal |
+                                          TrackableHitFlags.FeaturePoint |
+                                          TrackableHitFlags.PlaneWithinPolygon |
+                                          TrackableHitFlags.None |
+                                          TrackableHitFlags.PlaneWithinInfinity;
+
+        #endregion
+
+        /// <summary>
+        /// The Unity Update() method.
+        /// </summary>
+        public void Update()
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            this._QuitOnConnectionErrors();
+
+            // Check that motion tracking is tracking.
+            if (Session.Status != SessionStatus.Tracking)
+            {
+                const int lostTrackingSleepTimeout = 15;
+                Screen.sleepTimeout = lostTrackingSleepTimeout;
+                if (!this.m_IsQuitting && Session.Status.IsValid())
+                {
+                    this.SearchingForPlaneUI.SetActive(true);
+                }
+
+                return;
+            }
+
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+            // Iterate over planes found in this frame and instantiate corresponding GameObjects to visualize them.
+            Session.GetTrackables<TrackedPlane>(this.m_NewPlanes, TrackableQueryFilter.New);
+
+            for (int i = 0; i < this.m_NewPlanes.Count; i++)
+            {
+                // Instantiate a plane visualization prefab and set it to track the new plane. The transform is set to
+                // the origin with an identity rotation since the mesh for our prefab is updated in Unity World
+                // coordinates.
+
+                GameObject planeObject = Instantiate(this.TrackedPlanePrefab, Vector3.zero, Quaternion.identity, this.transform);
+                planeObject.GetComponent<TrackedPlaneVisualizer>().Initialize(this.m_NewPlanes[i]);
+
+
+            }
+
+            // Disable the snackbar UI when no planes are valid.
+            Session.GetTrackables<TrackedPlane>(this.m_AllPlanes);
+            bool showSearchingUI = true;
+            for (int i = 0; i < this.m_AllPlanes.Count; i++)
+            {
+                if (this.m_AllPlanes[i].TrackingState == TrackingState.Tracking)
+                {
+                    showSearchingUI = false;
+                    break;
+                }
+            }
+
+            this.SearchingForPlaneUI.SetActive(showSearchingUI);
+        }
+
+        //private void TEst()
+        //{
+            
+        //    // If the player has not touched the screen, we are done with this update.
+
+        //    Touch touch = Input.GetTouch(0);
+
+        //    if (Input.touchCount < 1 ||
+        //        (touch.phase != TouchPhase.Began &&
+        //        touch.phase != TouchPhase.Moved &&
+        //        touch.phase != TouchPhase.Stationary))
+        //    {
+        //        return;
+        //    }
+
+        //    // Raycast against the location the player touched to search for planes.
+        //    TrackableHit hit;
+        //    TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+        //                                      TrackableHitFlags.FeaturePointWithSurfaceNormal |
+        //                                      TrackableHitFlags.FeaturePoint |
+        //                                      TrackableHitFlags.PlaneWithinPolygon |
+        //                                      TrackableHitFlags.None |
+        //                                      TrackableHitFlags.PlaneWithinInfinity;
+
+            
+        //    // TODO SOME WHERE HERE BUGGG WAS SEEN
+        //    if (Frame.Raycast(touch.position.x, touch.position.y,raycastFilter , out hit))
+        //    {
+
+        //        RaycastHit hHit;
+        //        Ray ray = Camera.current.ScreenPointToRay(touch.position);
+
+
+        //        bool var = Physics.Raycast(ray, out hHit, 1000.0f);
+
+        //        Vector3 pos;
+
+        //        if (var)
+        //        {
+        //            pos = hHit.point + hHit.normal* this.AndyAndroidPrefab.transform.localScale.y / 2;
+        //        }
+        //        else
+        //        {
+        //            pos = new Vector3(hit.Pose.position.x, hit.Pose.position.y+ this.AndyAndroidPrefab.transform.localScale.y/2, hit.Pose.position.z);
+        //        }
+
+        //        pos /= this.AndyAndroidPrefab.transform.localScale.y;
+
+        //        pos.x = (float)Math.Round(pos.x, MidpointRounding.AwayFromZero);
+        //        pos.y = (float)Math.Round(pos.y, MidpointRounding.AwayFromZero);
+        //        pos.z = (float)Math.Round(pos.z, MidpointRounding.AwayFromZero);
+
+        //        pos *= this.AndyAndroidPrefab.transform.localScale.y;
+
+        //        Instantiate(this.AndyAndroidPrefab, pos, Quaternion.identity);
+        //    }
+        //}
+
+        
+        public void AddBlockFunc()
+        {          
+            // get position of center of the sreen
+            var cameraPos = this.GetCenterOfScreenPosition();
+
+            Vector3 pos =Vector3.zero;
+
+            // Physics //////////////////////////////////////////
+            TrackableHit hit;
+            RaycastHit hHit;
+            Ray ray = Camera.current.ScreenPointToRay(cameraPos);
+
+            if (Physics.Raycast(ray, out hHit, this.hitLength))
+            {
+                pos = hHit.point + hHit.normal* this.AndyAndroidPrefab.transform.localScale.y / 2;
+
+                Debug.Log("Unity My");
+            }
+            /////////////////////////////////////////////////////
+
+            // ARCore ///////////////////////////////////////////
+
+            else if (Frame.Raycast(cameraPos.x, cameraPos.y,raycastFilter , out hit))
+            {
+                pos = new Vector3(hit.Pose.position.x, hit.Pose.position.y+ this.AndyAndroidPrefab.transform.localScale.y/2, hit.Pose.position.z);
+
+                Debug.Log("Unity ACore");
+            }
+            /////////////////////////////////////////////////////
+
+            // Spawn cube
+            if (pos != Vector3.zero)
+            {
+                GameManager.InitiateBlockObject(this.AndyAndroidPrefab,this.RoundPosition(pos));
+            }
+        }
+
+        public void RemoveBlockFunc()
+        {
+            Vector3 pos =Vector3.zero;
+
+            // Physics //////////////////////////////////////////
+            RaycastHit hHit;
+            Ray ray = Camera.current.ScreenPointToRay(this.GetCenterOfScreenPosition());
+
+            if (Physics.Raycast(ray, out hHit, this.hitLength))
+            {
+                pos = hHit.point - hHit.normal* this.AndyAndroidPrefab.transform.localScale.y / 2;
+            }
+
+            // Remove cube
+            if (pos != Vector3.zero)
+            {
+                GameManager.DestroyBlockObject(this.RoundPosition(pos));
+            }
+        }
+
+        //private void TEst3()
+        //{
+            
+        //    // If the player has not touched the screen, we are done with this update.
+        //    Touch touch;
+
+        //    try
+        //    {
+        //        touch = Input.GetTouch(0);
+        //    }
+        //    catch (ArgumentException) {return;}
+        //    catch (IndexOutOfRangeException) {return;}
+
+        //    if (Input.touchCount < 1 ||touch.phase != TouchPhase.Began)
+        //    {
+        //        return;
+        //    }
+
+        //    // Raycast against the location the player touched to search for planes.
+
+        //    TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+        //                                      TrackableHitFlags.FeaturePointWithSurfaceNormal |
+        //                                      TrackableHitFlags.FeaturePoint |
+        //                                      TrackableHitFlags.PlaneWithinPolygon |
+        //                                      TrackableHitFlags.None |
+        //                                      TrackableHitFlags.PlaneWithinInfinity;
+
+
+
+        //    // get position of center of the sreen
+        //    var cameraPos = Camera.current.transform.position;
+        //    cameraPos.x += Screen.width / 2;
+        //    cameraPos.y += Screen.height / 2;
+
+        //    Vector3 pos =Vector3.zero;
+
+        //    // Physics //////////////////////////////////////////
+        //    TrackableHit hit;
+        //    RaycastHit hHit;
+        //    Ray ray = Camera.current.ScreenPointToRay(cameraPos);
+
+        //    if (Physics.Raycast(ray, out hHit, 1000.0f))
+        //    {
+        //        pos = hHit.point + hHit.normal* this.AndyAndroidPrefab.transform.localScale.y / 2;
+
+        //        Debug.Log("Unity My");
+        //    }
+        //    /////////////////////////////////////////////////////
+
+        //    // ARCore ///////////////////////////////////////////
+
+        //    else if (Frame.Raycast(cameraPos.x, cameraPos.y,raycastFilter , out hit))
+        //    {
+        //        pos = new Vector3(hit.Pose.position.x, hit.Pose.position.y+ this.AndyAndroidPrefab.transform.localScale.y/2, hit.Pose.position.z);
+
+        //        Debug.Log("Unity ACore");
+        //    }
+        //    /////////////////////////////////////////////////////
+
+        //    // Spawn cube
+        //    if (pos != Vector3.zero)
+        //    {
+        //        var cube = this.SpawnCube(pos);
+
+        //        //var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+        //        //// Make Andy model a child of the anchor.
+        //        //cube.transform.parent = anchor.transform;
+        //    }
+        //}
+
+        private Vector3 RoundPosition(Vector3 pos)
+        {
+            pos /= this.AndyAndroidPrefab.transform.localScale.y;
+
+            pos.x = (float)Math.Round(pos.x, MidpointRounding.AwayFromZero);
+            pos.y = (float)Math.Round(pos.y, MidpointRounding.AwayFromZero);
+            pos.z = (float)Math.Round(pos.z, MidpointRounding.AwayFromZero);
+
+            pos *= this.AndyAndroidPrefab.transform.localScale.y;
+
+            return pos;
+        }
+
+        #region Help
+
+        private Vector3 GetCenterOfScreenPosition()
+        {
+            var cameraPos = Camera.current.transform.position;
+            cameraPos.x += Screen.width / 2;
+            cameraPos.y += Screen.height / 2;
+
+            return cameraPos;
+        }
+
+        #endregion
+
+        #region Other
+
+        /// <summary>
+        /// Quit the application if there was a connection error for the ARCore session.
+        /// </summary>
+        private void _QuitOnConnectionErrors()
+        {
+            if (this.m_IsQuitting)
+            {
+                return;
+            }
+
+            // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+            if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+            {
+                this._ShowAndroidToastMessage("Camera permission is needed to run this application.");
+                this.m_IsQuitting = true;
+                this.Invoke("_DoQuit", 0.5f);
+            }
+            else if (Session.Status.IsError())
+            {
+                this._ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+                this.m_IsQuitting = true;
+                this.Invoke("_DoQuit", 0.5f);
+            }
+        }
+
+        /// <summary>
+        /// Actually quit the application.
+        /// </summary>
+        private void _DoQuit()
+        {
+            Application.Quit();
+        }
+
+        /// <summary>
+        /// Show an Android toast message.
+        /// </summary>
+        /// <param name="message">Message string to show in the toast.</param>
+        private void _ShowAndroidToastMessage(string message)
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            if (unityActivity != null)
+            {
+                AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+                unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+                {
+                    AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+                        message, 0);
+                    toastObject.Call("show");
+                }));
+            }
+        }
+        #endregion
+    }
+}
---
+++ Assets/AR/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using AR;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static void Log(object message)
+    {
+        Debug.Log($"Unity {message}");
+    }
+
+
+    public void AddBlock()
+    {
+        FindObjectOfType<ARController>().AddBlockFunc();
+    }
+
+    public void RemoveBlock()
+    {
+        FindObjectOfType<ARController>().RemoveBlockFunc();
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    private static Dictionary<Vector3, GameObject> blocks;
+
+    public static Dictionary<Vector3, GameObject> Blocks => blocks ?? (blocks = new Dictionary<Vector3, GameObject>());
+
+    public static GameObject InitiateBlockObject(GameObject prefab, Vector3 origin)
+    {
+        GameObject gameObj = null;
+
+        if (!Blocks.ContainsKey(origin))
+        {
+            gameObj = Instantiate(prefab, origin, Quaternion.identity);
+
+            GameManager.Blocks.Add(origin, gameObj);
+
+        }
+
+        return gameObj;
+    }
+
+    public static void DestroyBlockObject(Vector3 origin)
+    {
+        if (Blocks.ContainsKey(origin))
+        {
+            GameObject gm = Blocks[origin];
+
+            Blocks.Remove(origin);
+
+            Destroy(gm);
+        }
+    }
+    public static void DestroyBlockObject(GameObject gameObject) =>
+        DestroyBlockObject(gameObject.transform.position);
+}
